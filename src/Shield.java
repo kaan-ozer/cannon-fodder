c2524085cbce9b957d83e4c9671c721491da5fb7
selinunall <Seln01unnal@gmail.com>
2022-05-17 19:11:27 +0300

    added some methods to shield and cloth classes also improved tank and healer constructors

--- src/Shield.java
+++ src/Shield.java
@@ -1,4 +1,24 @@
-public class Shield {
+public  class Shield extends Item{
+
+    public Shield(String name,int weight,double value){
+        super(name, weight, value);
+    }
+
+    public double calculateDefence(Character character){
+      double defence=getValue() * character.getVitality();
+      return defence;
+    }
+
+    public void defence(Character chosenEnemy,Character chosenCharacter){
+        System.out.println( chosenCharacter.getRace() + " is defencing....");
+        System.out.println( chosenCharacter.getRace() + " defenced " + calculateDefence(chosenCharacter) + " damages from the " + chosenEnemy.getRace());
+
+
+    }
+
+
+
+
 
 
 }
